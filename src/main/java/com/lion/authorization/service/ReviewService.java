6612c4c83776400a8e001e56d601e8a5b92e89dc
KimGunWoo9595 <medneter@naver.com>
2022-12-06 14:23:08 +0900

    delete

--- src/main/java/com/lion/authorization/service/ReviewService.java
+++ src/main/java/com/lion/authorization/service/ReviewService.java
@@ -25,47 +25,5 @@ public class ReviewService {
     private final ReviewRepository reviewRepository;
     private final HospitalRepository hospitalRepository;
 
-    /*{
-  "hospitalId":10,
-  "title":"병원이 매우 크네요",
-  "content":"병원이 쾌적하고 좋아요",
-  "userName":"김건우"
-}*/
-    //리뷰 추가(병원정보+)
-    public ReviewCreateResponse add(ReviewCreateRequest reviewCreateRequest) {
-        //hospital 먼저 조회해주자
-        int hospitalId = reviewCreateRequest.getHospitalId();
-        Optional<Hospital> hospitalOptional = hospitalRepository.findById(hospitalId);
-        Hospital hospital = hospitalOptional.get(); // 해당 병원 객체
-
-        //dto -> entity
-        Review review = Review.builder()
-                .title(reviewCreateRequest.title)
-                .content(reviewCreateRequest.content)
-                .userName(reviewCreateRequest.userName)
-                .hospital(hospital).build();
-        //save(Review)
-        Review savedReview = reviewRepository.save(review);
-
-        ReviewCreateResponse reviewCreateResponse = new ReviewCreateResponse(savedReview);
-        return reviewCreateResponse;
-    }
-
-    //리뷰 단건 조회
-    public Review getReview(Long id) {
-        Optional<Review> reviewOptional = reviewRepository.findById(id);
-        Review review = reviewOptional.orElseThrow(() -> new RuntimeException("해당 id의 리뷰가 없습니다."));
-        return review;
-    }
-
-    //리뷰 전체 조회
-    public List<ReviewSelectResponse> findAll() {
-        List<Review> reviews = reviewRepository.findAll();
-
-        List<ReviewSelectResponse> reviewSelectResponses =
-                reviews.stream().map(r -> new ReviewSelectResponse(r)).collect(Collectors.toList());
-
-        return reviewSelectResponses;
-    }
-
+    
 }
