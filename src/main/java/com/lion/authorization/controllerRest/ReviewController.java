d971d513f0ead42834b7fc27ff6115c5fe9dd7cb
KimGunWoo9595 <medneter@naver.com>
2022-12-06 13:40:27 +0900

    리뷰 컨트롤러

--- src/main/java/com/lion/authorization/controllerRest/ReviewController.java
+++ src/main/java/com/lion/authorization/controllerRest/ReviewController.java
@@ -1,5 +1,4 @@
-package com.mustache.bbs.controllerRest;
-
+package com.lion.authorization.controllerRest;
 import com.mustache.bbs.domain.dto.review.ReviewCreateRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
@@ -12,11 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
 
-    //SpringSecurity에서 인증된 토큰을 얻은 User만 review를 쓸 수 있는 API
-    @PostMapping
-    public String write(@RequestBody ReviewCreateRequest dto, Authentication authentication) {
-        log.info("Controller user:{}", authentication.getName());
-        return "리뷰 등록에 성공 했습니다.";
 
-    }
+
 }
